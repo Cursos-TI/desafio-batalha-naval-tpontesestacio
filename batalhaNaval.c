b1233f0bd259a7451354c13040746cc4e83d6acf
tpontesestacio <202504434501@alunos.estacio.br>
2026-02-10 23:21:51 -0300

    Implementação do nível iniciante com as validações solicitadas

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -4,11 +4,87 @@
 // Este código inicial serve como base para o desenvolvimento do sistema de Batalha Naval.
 // Siga os comentários para implementar cada parte do desafio.
 
+int tabuleiro[10][10] = {
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+    };
+
+    // Navio horizontal
+    int navio1[3] = {3, 3, 3};
+    // Navio vertical
+    int navio2[3] = {3, 3, 3};
+    // Posições iniciais do navio 1
+    int posH1 = 6, posV1 = 0;
+    // Posições iniciais do navio 2
+    int posH2 = 7, posV2 = 2;
+
+void gerarTabuleiro(){
+    // Criar o tabuleiro
+    for(int i = 0; i < 10; i++){
+        for(int j = 0; j < 10; j++){
+            if(j < 9){
+                printf("[ %d ]", tabuleiro[i][j]);
+            } else{
+                printf("[ %d ]\n", tabuleiro[i][j]);
+            };
+        };
+    };
+
+    printf("\n");
+}
+
+int validarPosicaoNavio(int posicaoH, int tamanhoH, int i){
+    for(int x = posicaoH; x < posicaoH + tamanhoH; x++){
+        if(tabuleiro[i][x] != 0){
+            printf("A posição do navio coincide com a posição de outra embarcação. \n\n");
+            return 0; // Retorna falso
+        }
+    };
+    return 1; // Retorna verdadeiro
+}
+
+void posicionarNavios(int posicaoV, int tamanhoV, int posicaoH, int tamanhoH){
+
+    // Posicionar os navios no tabuleiro
+    for(int i = posicaoV; i < posicaoV + tamanhoV; i++){
+
+        // Testa se a posição do navio respeita os limites do tabuleiro 
+        if(posicaoH + tamanhoH > 10){
+            printf("O navio ultrapassa os limites do tabuleiro. \n\n");
+            break;
+        };
+
+        if(validarPosicaoNavio(posicaoH, tamanhoH, i) == 0){
+            break;
+        }else{
+
+            for(int j = posicaoH; j < posicaoH + tamanhoH; j++){
+                tabuleiro[i][j] = 3;
+            }
+        };
+        
+    };
+}
+
 int main() {
     // Nível Novato - Posicionamento dos Navios
     // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
     // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
     // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
+
+    // Configurar posição dos navios
+    posicionarNavios(posV1, 1, posH1, 3);
+    posicionarNavios(posV2, 3, posH2, 1);
+    // Gerar tabuleiro com as embarcações validadas
+    gerarTabuleiro();
 
     // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
     // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
